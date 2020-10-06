cb7812afcfd7b270f33a55fc3c45de474ac53197
Markus Hihn <0mbi@mailbox.org>
2020-10-06 06:42:48 +0200

    feat(ts/my-app): Call an API asynchronously
    - Fetch an API once and render it into JSON tree
    - Notice user while fetching is ongoing
    - Use monokai theme

--- lang/typescript/my-app/src/App.tsx
+++ lang/typescript/my-app/src/App.tsx
@@ -1,18 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 // import './App.css';
 import JSONTree from 'react-json-tree'
 
-
+// https://stackoverflow.com/questions/37764665/typescript-sleep
+function delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) )
+}
 
 function App() {
-  const a_json = {
-    array: [1, 2, 3],
-    bool: true,
-    object: {
-      foo: 'bar'
-    }
-  }
+  const [apiFetched, setApiFetched] = useState(false)
+  const [ajson, setAjson] = useState("")
   const theme = {
     scheme: 'monokai',
     author: 'wimer hazenberg (http://www.monokai.nl)',
@@ -33,9 +31,25 @@ function App() {
     base0E: '#ae81ff',
     base0F: '#cc6633',
   };
+  // add an empty list for calling useEffect only on first render
+  // https://stackoverflow.com/questions/55484033/reactjs-how-to-call-useeffect-hook-only-once-to-fetch-api-data
+  // https://reactjs.org/docs/hooks-faq.html#what-can-i-do-if-my-effect-dependencies-change-too-often
+  useEffect(() => {
+    console.log('called use effect')
+    const api =  'http://dummy.restapiexample.com/api/v1/employees'
+    fetch(api)
+      .then(response => response.json())
+      .then(res => {
+        setAjson(res)
+        delay(3000).then(any=>setApiFetched(true)) // simulate longer loading
+      })
+      .catch((error: any) => console.log(error.message)) // logs any error from the promise
+  }, [])
   return (
-    <div className="App">
-      <JSONTree data={a_json} />
+    <div>
+      <JSONTree data={ajson} theme={theme} />
+      <p>Fetching API: {apiFetched?"false":"true"}</p>
+      <p>Finished fetching: {apiFetched?"true":"false"}</p>
     </div>
   );
 }
