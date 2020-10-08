f8341302c21598ced266393f219b8bf335c90d60
Markus Hihn <markus.hihn@dcso.de>
2020-10-08 16:02:27 +0200

    feat(typescrypt/my-app): Add a simple functional component
    - Add a simple functional component
    - Remove the JSONTree theme (for better readability of code)

--- lang/typescript/my-app/src/App.tsx
+++ lang/typescript/my-app/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, FunctionComponent } from 'react';
 import logo from './logo.svg';
 // import './App.css';
 import JSONTree from 'react-json-tree'
@@ -8,29 +8,45 @@ function delay(ms: number) {
     return new Promise( resolve => setTimeout(resolve, ms) )
 }
 
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+  stock: number;
+}
+
+interface ProductProps {
+  products: Product[];
+}
+
+const ProductTable: FunctionComponent<ProductProps> = ({products}) => <aside>
+    <table>
+      <caption>Our products</caption>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+          <th>In Stock</th>
+        </tr>
+      </thead>
+      <tbody>
+        {products.map((product:Product) => (
+          <tr key={product.id}>
+            <td>{product.name}</td>
+            <td>{product.price}</td>
+            <td>{product.stock}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+</aside>
+
+
 function App() {
   const [apiFetched, setApiFetched] = useState(false)
   const [ajson, setAjson] = useState("")
-  const theme = {
-    scheme: 'monokai',
-    author: 'wimer hazenberg (http://www.monokai.nl)',
-    base00: '#272822',
-    base01: '#383830',
-    base02: '#49483e',
-    base03: '#75715e',
-    base04: '#a59f85',
-    base05: '#f8f8f2',
-    base06: '#f5f4f1',
-    base07: '#f9f8f5',
-    base08: '#f92672',
-    base09: '#fd971f',
-    base0A: '#f4bf75',
-    base0B: '#a6e22e',
-    base0C: '#a1efe4',
-    base0D: '#66d9ef',
-    base0E: '#ae81ff',
-    base0F: '#cc6633',
-  };
+  const products : Product[] = [{id: 1, name: 'milk', price: 2, stock: 7}];
+
   // add an empty list for calling useEffect only on first render
   // https://stackoverflow.com/questions/55484033/reactjs-how-to-call-useeffect-hook-only-once-to-fetch-api-data
   // https://reactjs.org/docs/hooks-faq.html#what-can-i-do-if-my-effect-dependencies-change-too-often
@@ -47,9 +63,10 @@ function App() {
   }, [])
   return (
     <div>
-      <JSONTree data={ajson} theme={theme} />
+      <JSONTree data={ajson} />
       <p>Fetching API: {apiFetched?"false":"true"}</p>
       <p>Finished fetching: {apiFetched?"true":"false"}</p>
+      <ProductTable products={products}></ProductTable>
     </div>
   );
 }
